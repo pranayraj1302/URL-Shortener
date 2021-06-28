5d75e3d888264a1c9f66de9eb8f2a9c6a6a532d9
pranayraj1302 <52375123+pranayraj1302@users.noreply.github.com>
2021-06-28 14:34:24 +0530

    Add files via upload

---
+++ url_short.py
@@ -0,0 +1,16 @@
+import pyshorteners as ps
+
+'''url = "https://www.amazon.in/dp/1638738122"
+u = ps.Shortener().tinyurl.short(url)
+
+print(u)
+
+url2 = "https://medium.com/@authorpranayraj"
+u2 = ps.Shortener().tinyurl.short(url2)
+
+print(u2)'''
+
+url3 = "https://www.instagram.com/the_spilled_author/"
+u3 = ps.Shortener().tinyurl.short(url3)
+
+print(u3)
